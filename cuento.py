820b9e9981a5d0f019f50aec999f02692863eae0
Luis Barria <luis.barriar@uip.edu.pa>
2026-06-20 12:55:06 -0500

    Añadido módulo de cuento

---
+++ cuento.py
@@ -0,0 +1,6 @@
+def imprimir_cuento():
+    print("Había una vez un estudiante llamado Luis.")
+    print("Luis aprendió GitHub y Python.")
+    print("Con esfuerzo terminó su proyecto exitosamente.")
+
+imprimir_cuento()
