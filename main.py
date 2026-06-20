2e94196ce3ebbb1b42ad46fa3e030fbe7a98b859
Luis Barria <luis.barriar@uip.edu.pa>
2026-06-20 12:58:07 -0500

    Añadido archivo principal

---
+++ main.py
@@ -0,0 +1,11 @@
+from operaciones import suma, resta, multiplicacion, division
+from cuento import imprimir_cuento
+
+print("=== Operaciones Matemáticas ===")
+print("Suma:", suma(10, 5))
+print("Resta:", resta(10, 5))
+print("Multiplicación:", multiplicacion(10, 5))
+print("División:", division(10, 5))
+
+print("\n=== Cuento ===")
+imprimir_cuento()
