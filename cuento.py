b1e034487ef045a459cd31b9611eadbc9fbe11fa
Maria Barilla <maria.barilla@uip.edu.pa>
2026-06-27 22:45:06 -0500

    Agregar una linea final al cuento
    Se agrego una linea al final del cuento para mejorar su cierre

--- cuento.py
+++ cuento.py
@@ -2,5 +2,6 @@ def imprimir_cuento():
     print("Había una vez un estudiante llamado Luis.")
     print("Luis aprendió GitHub y Python.")
     print("Con esfuerzo terminó su proyecto exitosamente.")
+    print("Finalmente compartió su proyecto con otros desarrolladores.")
 
 imprimir_cuento()
