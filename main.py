7ea884643e27a89f108ee68542055ddc21c616ed
jesusaldeano <jesus.aldeano@uip.edu.pa>
2026-07-06 10:51:21 -0500

    Add separator line in main.py output

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Resta:", resta(10, 5))
 print("Multiplicación:", multiplicacion(10, 5))
 print("División:", division(10, 5))
 
-print("\n" + "="*30)  # ← línea agregada
+print("\n" + "="*30)  # línea agregada
 
 print("\n=== Cuento ===")
 imprimir_cuento()
